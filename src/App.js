51084309664400ca1facb050b391a05a0e0cc2d1
nazmul <nazmulislam.ni897@gmail.com>
2022-06-08 15:10:32 +0600

    creating a navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./Container";
+import { Navbar } from "./Components";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
