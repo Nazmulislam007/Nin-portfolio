523f2d449f6bb50ff57b46b75cc6bf886f7af60d
nazmul <nazmulislam.ni897@gmail.com>
2022-06-09 22:32:19 +0600

    about section

--- src/Container/About/About.js
+++ src/Container/About/About.js
@@ -1,8 +1,60 @@
 import React from "react";
 import "./About.scss";
+import { motion } from "framer-motion";
+import { img } from "../../Constants";
 
 const About = () => {
-  return <div>About</div>;
+  const abouts = [
+    {
+      title: "Web Developer",
+      description: "I am a good Web Developer.",
+      imageUrl: img.about01,
+    },
+    {
+      title: "Front-End Web Developer",
+      description: "I am a good Front-End Web Developer.",
+      imageUrl: img.about02,
+    },
+    {
+      title: "Firebase Backend Developer",
+      description: "I am a good Firebase Backend Developer.",
+      imageUrl: img.about03,
+    },
+    {
+      title: "ReactJS Developer",
+      description: "I am a good ReactJS Developer.",
+      imageUrl: img.about04,
+    },
+  ];
+
+  return (
+    <div className="app__about app__whitebg app__flex">
+      <h2 className="head-text">
+        I Know that <span>Good Developer</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((abouts, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={abouts.title + index}
+          >
+            <img src={abouts.imageUrl} alt={abouts.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {abouts.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {abouts.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Container/Header/Header.js
+++ src/Container/Header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={img.profile} alt="profile-bg" />
+        <img src={img.profile} className="profile-img" alt="profile-bg" />
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
