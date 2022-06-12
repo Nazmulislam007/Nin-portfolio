e855b132bd6891944db2941b967f3ec8021a09bc
nazmul <nazmulislam.ni897@gmail.com>
2022-06-13 02:37:26 +0600

    work section part 1

---
+++ src/Constants/WorksApi.js
@@ -0,0 +1,39 @@
+import img from "./Images";
+
+export const data = [
+  {
+    image: img.about01,
+    title: "Resturant Projects",
+    description: "HTML, CSS3, JAVASCRIPT",
+    projectLink: "https://nazmulislam007.github.io/chicken-food-web/",
+    codeLink: "https://github.com/Nazmulislam007/chicken-food-web",
+    tags: ["Static", "All"],
+  },
+  // {
+  //
+  //   image: img.about01,
+  //   title: "",
+  //   description: "",
+  //   projectLink: "",
+  //   codeLink: "",
+  //   tags: "",
+  // },
+  // {
+  //
+  //   image: img.about01,
+  //   title: "",
+  //   description: "",
+  //   projectLink: "",
+  //   codeLink: "",
+  //   tags: "",
+  // },
+  // {
+  //
+  //   image: img.about01,
+  //   title: "",
+  //   description: "",
+  //   projectLink: "",
+  //   codeLink: "",
+  //   tags: "",
+  // },
+];
